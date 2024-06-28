f34e3efb12d866c4765b555a932dbf5ca5272746
bvithusan1999 <bvithushan1999@gmail.com>
2024-06-28 10:00:01 +0530

    docker-works-001

--- client/src/index.js
+++ client/src/index.js
@@ -3,20 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import store from "./store/ReduxStore";
 import { Provider } from "react-redux";
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <Provider store = {store}>
+  <Provider store={store}>
     <BrowserRouter>
       <Routes>
-        <Route path="*" element ={<App/>} />
-
+        <Route path="*" element={<App />} />
       </Routes>
     </BrowserRouter>
-    
-
   </Provider>,
-  
+
   document.getElementById("root")
 );
 
